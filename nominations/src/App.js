8a83508eb42bcb6aabc3907afab059d6e3efd6c8
Michelle <michieugwu@gmail.com>
2023-10-14 19:36:54 +0100

    imported react-query (tanstack)

--- nominations/src/App.js
+++ nominations/src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './styles/App.css';
 import './styles/StylesToBeTranslated.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import Intro from './pages/Intro';
 import CreateNom from './pages/CreateNomination';
 import Reason from './pages/Reason';
@@ -11,20 +15,23 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <div className="h-screen bg-blue-500">
-      <Header />
-      <h2>The different pages will go here</h2>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Intro />} />
-          <Route path='/intro' element={<Intro />} />
-          <Route path='/create-nomination' element={<CreateNom />} />
-          <Route path='/reason' element={<Reason />} />
-          <Route path='*' element={<NoPage />} />
-        </Routes>
-      </BrowserRouter>
-      {/* <header className="App-header">
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <h2>The different pages will go here</h2>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Intro />} />
+            <Route path='/intro' element={<Intro />} />
+            <Route path='/create-nomination' element={<CreateNom />} />
+            <Route path='/reason' element={<Reason />} />
+            <Route path='*' element={<NoPage />} />
+          </Routes>
+        </BrowserRouter>
+        {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -38,7 +45,8 @@ function App() {
           Learn Reactyyy
         </a>
       </header> */}
-      <Footer />
+        <Footer />
+      </QueryClientProvider>
     </div>
   );
 }
