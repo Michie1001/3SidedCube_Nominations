a6905125aa345cc2f82960a87f34c1b284a08493
Michelle <michieugwu@gmail.com>
2023-10-15 03:17:42 +0100

    Reason page

--- nominations/src/pages/Reason.js
+++ nominations/src/pages/Reason.js
@@ -1,6 +1,38 @@
+import { Link } from 'react-router-dom';
+import jumbo from '../images/reason/reason3.png';
+
 export default function Reason() {
 
     return (
-        <h3>The reason page</h3>
+        <>
+            <div className="bg h-screen w-screen flex justify-center">
+                <div className="container max-w-2xl intro-screen">
+                    <div className='jumbo-container'>
+                        <img src={jumbo} alt="Jumbo image of the cube office" />
+                    </div>
+                    <div className='box-text'>
+                        <h2>I’d like to nominate DAVID because...</h2>
+                        <p>Please let us know why you think this cube deserves the ‘cube of the month’ title 🏆⭐</p>
+                        <div>
+                            <label><b>Reasoning</b></label>
+                            <input placeholder="Enter reason here"></input>
+                        </div>
+                        <div className='navigation flex justify-between'>
+                            <Link to="/create-nomination" className='flex justify-center'>
+                                <button type="button" className='button-secondary-active w-24'>
+                                    BACK
+                                </button>
+                            </Link>
+                            <Link to="/reason" className='flex justify-center'>
+                                <button type="button" className='button-primary-inactive w-40'>
+                                    NEXT
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
     );
 }
